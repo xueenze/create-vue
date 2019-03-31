7afe0d44f047eee4c1d216944cbddb8b98bc3e80
xueenze <kameleon@126.com>
2019-03-31 12:53:08 +0800

    修改axios的使用方式！

--- src/App.vue
+++ src/App.vue
@@ -13,27 +13,6 @@ export default {
     },
     created() {
         console.log(process.env.NODE_ENV);
-        // get 请求
-        this.$ajax
-            .get(
-                'https://www.easy-mock.com/mock/5b1f88a39f7d4a3a70fcbfd3/suporka/get',
-                {}
-            )
-            .then(res => {
-                console.log(res);
-            });
-        // post 请求
-        this.$ajax
-            .post(
-                'https://www.easy-mock.com/mock/5b1f88a39f7d4a3a70fcbfd3/suporka/post',
-                {}
-            )
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err);
-            });
     },
     methods: {}
 };
